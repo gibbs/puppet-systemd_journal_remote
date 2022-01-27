930ca7588004a6fa02709f380a2d730f0335bf06
Dan Gibbs <daniel.gibbs@gmail.com>
2022-01-27 14:40:00 +0000

    Add AlmaLinux and Rocky Linux support

--- spec/acceptance/include_remote_spec.rb
+++ spec/acceptance/include_remote_spec.rb
@@ -1,6 +1,15 @@
 require 'spec_helper_acceptance'
 
 describe 'include the systemd_journal_system::remote class' do
+  # @note: Almalinux and Rocky Linux images missing ss utility
+  if os[:family] == 'redhat'
+    apply_manifest(%(
+      package { 'iproute':
+        ensure => installed,
+      }
+    ), catch_failures: true)
+  end
+
   pp = <<-MANIFEST
     include ::systemd_journal_remote::remote
   MANIFEST
